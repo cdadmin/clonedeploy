1351394dcd3fb6f50f1916d5279ba4da12e8fe6e
cdadmin <crucibleadmin@cruciblewds.org>
2016-04-04 09:02:00 -0400

    started munki integration

--- clonedeploy_web/web/App_Code/BLL/Workflows/DynamicPartitionScript.cs
+++ clonedeploy_web/web/App_Code/BLL/Workflows/DynamicPartitionScript.cs
@@ -296,7 +296,7 @@ namespace BLL.Workflows
                                   partition.Size + "s ";
             }
 
-            partitionScript += "\"" + "Free Space" + "\"" + " " + "\"" + "" + "\"" + " " + "R" + "\" > /tmp/createPartitions";
+            partitionScript += "\'" + "Free Space" + "\"" + " " + "\"" + "" + "\"" + " " + "R" + "\' > /tmp/createPartitions";
                                   
             foreach (var part in from part in ImageSchema.HardDrives[HdNumberToGet].Partitions
                                  where part.Active
---
+++ clonedeploy_web/web/views/global/munki/munki.master.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class views_global_munki_munki : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
