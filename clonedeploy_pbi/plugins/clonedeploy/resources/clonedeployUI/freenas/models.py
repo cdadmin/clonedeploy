6b66cdcd882913b7739d40d98d4a3625886f503a
cdadmin <crucibleadmin@cruciblewds.org>
2016-10-02 14:30:38 -0400

    remove freenas pbi - deprecated
    FreeNAS 10 does not use plugins or jails, no reason to continue
    development

--- clonedeploy_pbi/plugins/clonedeploy/resources/clonedeployUI/freenas/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Clonedeploy(models.Model):
-    """
-    Django model describing every tunable setting for clonedeploy
-    """
-
-    enable = models.BooleanField(default=False)
--- clonedeploy_pbi/plugins/clonedeploy/resources/dbsetup.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-mysqladmin create clonedeploy
-mysql clonedeploy < ${clonedeploy_pbi_path}/clonedeploy/cd.sql
-mysqladmin -u root password xx_marker1_xx
-
