105887ad4a831cdf0e49ce446f4f84600799a402
cdadmin <cdadmin@clonedeploy.org>
2018-09-07 23:32:52 -0400

    add profile templates continued

---
+++ CloneDeploy-Entities/ImageProfileTemplate.cs
@@ -0,0 +1,120 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CloneDeploy_Common.Enum;
+
+namespace CloneDeploy_Entities
+{
+    [Table("image_profile_templates")]
+    public class ImageProfileTemplate
+    {
+        [Column("profile_boot_image", Order = 6)]
+        public string BootImage { get; set; }
+
+        [Column("change_name", Order = 28)]
+        public int ChangeName { get; set; }
+
+        [Column("compression_algorithm", Order = 20)]
+        public string Compression { get; set; }
+
+        [Column("compression_level", Order = 21)]
+        public string CompressionLevel { get; set; }
+
+        [Column("custom_partition_script", Order = 19)]
+        public string CustomPartitionScript { get; set; }
+
+        [Column("custom_image_schema", Order = 22)]
+        public string CustomSchema { get; set; }
+
+        [Column("custom_upload_schema", Order = 23)]
+        public string CustomUploadSchema { get; set; }
+
+        [Column("profile_description", Order = 3)]
+        public string Description { get; set; }
+
+        [Column("fix_bcd", Order = 15)]
+        public int FixBcd { get; set; }
+
+        [Column("fix_bootloader", Order = 16)]
+        public int FixBootloader { get; set; }
+
+        [Column("force_dynamic_partitions", Order = 18)]
+        public int ForceDynamicPartitions { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("image_profile_template_id", Order = 1)]
+        public int Id { get; set; }
+
+        [Column("profile_kernel", Order = 5)]
+        public string Kernel { get; set; }
+
+        [Column("profile_kernel_arguments", Order = 7)]
+        public string KernelArguments { get; set; }
+
+        [Column("profile_name", Order = 2)]
+        public string Name { get; set; }
+
+        [Column("osx_target_volume", Order = 29)]
+        public string OsxTargetVolume { get; set; }
+
+        [Column("partition_method", Order = 17)]
+        public string PartitionMethod { get; set; }
+
+        [Column("multicast_receiver_arguments", Order = 26)]
+        public string ReceiverArguments { get; set; }
+
+        [Column("remove_gpt_structures", Order = 11)]
+        public int RemoveGPT { get; set; }
+
+        [Column("multicast_sender_arguments", Order = 25)]
+        public string SenderArguments { get; set; }
+
+        [Column("skip_set_clock", Order = 9)]
+        public int SkipClock { get; set; }
+
+        [Column("skip_core_download", Order = 8)]
+        public int SkipCore { get; set; }
+
+        [Column("skip_volume_expand", Order = 14)]
+        public int SkipExpandVolumes { get; set; }
+
+        [Column("skip_lvm_shrink", Order = 13)]
+        public int SkipShrinkLvm { get; set; }
+
+        [Column("skip_volume_shrink", Order = 12)]
+        public int SkipShrinkVolumes { get; set; }
+
+        [Column("task_completed_action", Order = 10)]
+        public string TaskCompletedAction { get; set; }
+
+        [Column("upload_schema_only", Order = 24)]
+        public int UploadSchemaOnly { get; set; }
+
+        [Column("web_cancel", Order = 27)]
+        public int WebCancel { get; set; }
+
+        [Column("wim_enabled_multicast", Order = 34)]
+        public int WimMulticastEnabled { get; set; }
+
+        [Column("skip_nvram", Order = 35)]
+        public int SkipNvramUpdate { get; set; }
+
+        [Column("randomize_guids", Order = 36)]
+        public int RandomizeGuids { get; set; }
+
+        [Column("force_standard_efi", Order = 37)]
+        public int ForceStandardEfi { get; set; }
+
+        [Column("force_standard_legacy", Order = 38)]
+        public int ForceStandardLegacy { get; set; }
+
+        [Column("simple_upload_schema", Order = 39)]
+        public int SimpleUploadSchema { get; set; }
+
+        [Column("erase_partitions", Order = 40)]
+        public int ErasePartitions { get; set; }
+
+        [Column("template_type")]
+        public  EnumProfileTemplate.TemplateType TemplateType { get; set; }
+    }
+}
---
+++ CloneDeploy-Helpers/Enum/EnumProfileTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneDeploy_Common.Enum
+{
+    public class EnumProfileTemplate
+    {
+        public enum TemplateType
+        {
+            LinuxBlock = 0,
+            LinuxFile = 1,
+            MacOS = 2,
+            WinPE = 3,
+        }
+    }
+}
